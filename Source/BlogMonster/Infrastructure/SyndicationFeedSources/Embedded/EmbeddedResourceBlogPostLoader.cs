35548cb44ad0c7d2324db78affb231a834f0fc77
uglybugger <andrewh@uglybugger.org>
2014-02-16 15:44:05 +1000

    Reintroduced .AsParallel().

--- Source/BlogMonster/Infrastructure/SyndicationFeedSources/Embedded/EmbeddedResourceBlogPostLoader.cs
+++ Source/BlogMonster/Infrastructure/SyndicationFeedSources/Embedded/EmbeddedResourceBlogPostLoader.cs
@@ -58,6 +58,7 @@ namespace BlogMonster.Infrastructure.SyndicationFeedSources.Embedded
         private IEnumerable<SyndicationItem> LoadSyndicationItemsFromAssembly(Assembly assembly)
         {
             var syndicationItems = assembly.GetManifestResourceNames()
+                                           .AsParallel()
                                            .Where(_blogPostResourceNameFilter)
                                            .Select(resourceName => TryLoadSyndicationItem(resourceName, assembly))
                                            .NotNull()
