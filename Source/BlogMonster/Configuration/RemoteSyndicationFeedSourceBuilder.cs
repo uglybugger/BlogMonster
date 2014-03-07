74d2d693b2a12bd0a6610ad04ffe07a193f21fe6
uglybugger <andrewh@uglybugger.org>
2014-03-07 15:36:59 +1000

    Added ability to filter remote feed items

--- Source/BlogMonster/Configuration/RemoteSyndicationFeedSourceBuilder.cs
+++ Source/BlogMonster/Configuration/RemoteSyndicationFeedSourceBuilder.cs
@@ -1,4 +1,5 @@
 using System;
+using System.ServiceModel.Syndication;
 using BlogMonster.Infrastructure;
 using BlogMonster.Infrastructure.SyndicationFeedSources.Remote;
 
@@ -9,12 +10,13 @@ namespace BlogMonster.Configuration
         public RemoteSyndicationFeedSourceBuilder(Uri feedUri)
         {
             FeedUri = feedUri;
-
             CacheTimeout = TimeSpan.FromMinutes(10);
+            Filter = si => true;
         }
 
         internal Uri FeedUri { get; private set; }
         internal TimeSpan CacheTimeout { get; set; }
+        internal Func<SyndicationItem, bool> Filter { get; private set; }
 
         public RemoteSyndicationFeedSourceBuilder WithCacheTimeout(TimeSpan cacheTimeout)
         {
@@ -22,10 +24,16 @@ namespace BlogMonster.Configuration
             return this;
         }
 
+        public RemoteSyndicationFeedSourceBuilder WithFilter(Func<SyndicationItem, bool> filter)
+        {
+            Filter = filter;
+            return this;
+        }
+
         public RemoteSyndicationFeedSource Grr()
         {
             var clock = new SystemClock();
-            return new RemoteSyndicationFeedSource(clock, CacheTimeout, FeedUri);
+            return new RemoteSyndicationFeedSource(clock, CacheTimeout, FeedUri, Filter);
         }
     }
 }
--- Source/BlogMonster/Infrastructure/SyndicationFeedSources/Remote/RemoteSyndicationFeedSource.cs
+++ Source/BlogMonster/Infrastructure/SyndicationFeedSources/Remote/RemoteSyndicationFeedSource.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Linq;
+using System.Reflection;
 using System.ServiceModel.Syndication;
 using System.Xml;
 
@@ -8,10 +10,12 @@ namespace BlogMonster.Infrastructure.SyndicationFeedSources.Remote
     {
         private readonly Cached<SyndicationFeed> _feed;
         private readonly Uri _feedUri;
+        private readonly Func<SyndicationItem, bool> _filter;
 
-        public RemoteSyndicationFeedSource(IClock clock, TimeSpan cacheTimeout, Uri feedUri)
+        public RemoteSyndicationFeedSource(IClock clock, TimeSpan cacheTimeout, Uri feedUri, Func<SyndicationItem, bool> filter)
         {
             _feedUri = feedUri;
+            _filter = filter;
             _feed = new Cached<SyndicationFeed>(cacheTimeout, clock, Fetch);
         }
 
@@ -25,6 +29,12 @@ namespace BlogMonster.Infrastructure.SyndicationFeedSources.Remote
             using (var reader = XmlReader.Create(_feedUri.ToString()))
             {
                 var feed = SyndicationFeed.Load(reader);
+                var filteredItems = feed.Items
+                                        .Where(item => _filter(item))
+                                        .ToArray();
+
+                var itemsField = feed.GetType().GetField("items", BindingFlags.Instance | BindingFlags.NonPublic);
+                itemsField.SetValue(feed, filteredItems);
                 return feed;
             }
         }
